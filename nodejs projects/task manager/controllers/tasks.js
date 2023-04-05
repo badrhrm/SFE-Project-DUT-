de70517bd6c2b94bef5af674c5fe3cb8bed9ef18
Badr Eddine Hrimech <badr.eddine.hrimech@proton.me>
2023-04-05 20:33:34 +0000

    second commit, before trying  delete and patch methods

--- nodejs projects/task manager/controllers/tasks.js
+++ nodejs projects/task manager/controllers/tasks.js
@@ -1,10 +1,21 @@
-const getAllTasks = (req, res) => {
-  res.send("all items ");
+const Task = require("../models/Task");
+const getAllTasks = async (req, res) => {
+  //res.send("all items ");
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks }); //~json({tasks:tasks})
+  } catch (error) {}
 };
 
-const createTask = (req, res) => {
+const createTask = async (req, res) => {
   //res.send('create task ');
-  res.json(req.body);
+  //res.json(req.body);
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const getTask = (req, res) => {
