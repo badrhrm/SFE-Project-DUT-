1ffe0b0e089786d77f5fc22cb8c3be094419a1e7
Badr Eddine Hrimech <badr.eddine.hrimech@proton.me>
2023-04-05 22:57:26 +0000

    all methods done

--- nodejs projects/task manager/controllers/tasks.js
+++ nodejs projects/task manager/controllers/tasks.js
@@ -35,8 +35,21 @@ const getTask = async (req, res) => {
   }
 };
 
-const updateTask = (req, res) => {
-  res.send("update all tasks");
+const updateTask = async (req, res) => {
+  //res.send("update all tasks");
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const deleteTask = async (req, res) => {
@@ -49,7 +62,7 @@ const deleteTask = async (req, res) => {
     }
     //here we have either one of the 3 and the first is the most used
     //res.status(200).send(); //here we just send the response that the request succeded
-    res.status(200).json({ task: null, status: "success" });
+    res.status(200).json({ task: null, status: "success" }); // i like this one
     //res.status(200).json(task); //here we send ourselves the tasks'info just to tell us that it was deleted though it is not needed at all and this way is not used
   } catch (error) {
     res.status(500).json({ msg: error });
