eb42dfb5be252cb6aeaa14f1f4064ac9a69b0592
Badr Eddine Hrimech <badr.eddine.hrimech@proton.me>
2023-04-09 19:29:16 +0000

    making proper controller for registering accounts and haching passwords in user model

--- nodejs projects/jobs api/controllers/auth.js
+++ nodejs projects/jobs api/controllers/auth.js
@@ -1,6 +1,19 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+//const {BadRequestError} = require("../errors");
+
 const register = async (req, res) => {
-  res.send("register use");
+  // if not using mongo validator (required :true) then i must add this + import Error class:
+  //const { name, email, password } = req.body;
+  // if (!name || !email || !password) {
+  //   throw new BadRequestError("You must provide the name, email and password");
+  // }
+
+  const user = await User.create({ ...req.body });
+  res.status(StatusCodes.CREATED).json({ user }); //201
+  //res.send("register use");
 };
+
 const login = async (req, res) => {
   res.send("login use");
 };
--- nodejs projects/jobs api/models/User.js
+++ nodejs projects/jobs api/models/User.js
@@ -1,15 +1,16 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   name: {
-    type: string,
+    type: String,
     required: [true, "must enter the name"],
-    minlength: 2,
     maxlength: 30,
+    minlength: 2,
     lowercase: true,
   },
   email: {
-    type: string,
+    type: String,
     required: [true, "must enter the gmail"],
     match: [
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
@@ -17,10 +18,18 @@ const UserSchema = new mongoose.Schema({
     ],
     unique: true,
   },
-  passowrd: {
-    type: string,
+  password: {
+    type: String,
     required: [true, "must enter the password"],
     minlength: 6,
   },
 });
+
+//this syntax provided by mongo
+//this.password refers to only password in this file
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 module.exports = mongoose.model("User", UserSchema);
