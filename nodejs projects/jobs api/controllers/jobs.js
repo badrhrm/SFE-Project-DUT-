97f40946dd267ad692f64cf86f44303ef426b469
Badr Eddine Hrimech <badr.eddine.hrimech@proton.me>
2023-04-11 14:09:28 +0000

    added update(patch) and delete controllers

--- nodejs projects/jobs api/controllers/jobs.js
+++ nodejs projects/jobs api/controllers/jobs.js
@@ -38,11 +38,42 @@ const createJob = async (req, res) => {
 };
 
 const updateJob = async (req, res) => {
-  res.send("update jobs");
+  const {
+    body: { company, position },
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  if (company === "" || position === "") {
+    throw new BadRequestError("Company or Position fields cannot be empty");
+  }
+  const job = await Job.findByIdAndUpdate(
+    { _id: jobId, createdBy: userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
+  //res.send("update jobs");
 };
 
 const deleteJob = async (req, res) => {
-  res.send("delete  job ");
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findByIdAndRemove({
+    _id: jobId,
+    createdBy: userId,
+  });
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).send("deleted");
+  //res.send("delete  job ");
 };
 
 module.exports = {
