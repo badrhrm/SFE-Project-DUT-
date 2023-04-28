58756d243f9305439ad32d7791b8f6e5b78f6d23
Badr Eddine Hrimech <badr.eddine.hrimech@proton.me>
2023-04-28 17:30:37 +0100

    testing post method and trying custom middle ware to understand the express.json() one

--- mern projects/task-manager-app/backend/server.js
+++ mern projects/task-manager-app/backend/server.js
@@ -3,13 +3,29 @@ const app = express();
 const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
 
+//express middle to get json data (req,body) from req from header of URL
+app.use(express.json());
+
 //routes
 app.get("/", (req, res) => {
   res.send("HELLO");
   console.log("hello");
 });
 
-const PORT = process.env.PORT || 3999;
+//middleware
+const logger = (req, res, next) => {
+  console.log("middleware");
+  console.log(req.method);
+  next();
+};
+
+app.post("/api/v1/tasks", logger, async (req, res) => {
+  console.log("tasks");
+  console.log(req.body);
+  res.send("task created");
+});
+
+const PORT = process.env.PORT || 3777;
 
 const startServer = async () => {
   try {
@@ -25,3 +41,13 @@ const startServer = async () => {
 };
 
 startServer();
+
+// //2nd method to connect db and server
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => {
+//     app.listen(PORT, () => {
+//       console.log(`server is listening to ${PORT}...`);
+//     });
+//   })
+//   .catch((error) => console.log(error));
