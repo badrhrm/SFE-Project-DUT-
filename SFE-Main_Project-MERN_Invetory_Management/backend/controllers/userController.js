4df492fda4a324f4c531b3367feea6eea9ff75fb
Badr Eddine Hrimech <badr.eddine.hrimech@proton.me>
2023-05-04 14:23:08 +0100

    logout and the get used API endpoint is implimented in userController

--- SFE-Main_Project-MERN_Invetory_Management/backend/controllers/userController.js
+++ SFE-Main_Project-MERN_Invetory_Management/backend/controllers/userController.js
@@ -115,4 +115,35 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { registerUser, loginUser };
+// Logout
+// to logout(remove token) we have 2 options either delete cookie from frontend or expire the cookie because it contains the token
+const logout = asyncHandler(async (req, res) => {
+  res.cookie("token", "", {
+    httpOnly: true,
+    expires: new Date(0),
+    secure: true,
+    sameSite: "none",
+  });
+  return res.status(200).json({ message: "Successfully Logged Out" });
+});
+
+const getUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  // console.log(user);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      photo: user.photo,
+      phone: user.phone,
+      bio: user.bio,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User Not Found");
+  }
+});
+
+module.exports = { registerUser, loginUser, logout, getUser };
